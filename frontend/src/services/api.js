42100822a6f0ba3d4f26a8ed989fb075376a1ebe
Iskander Shakirov <shakiroff@MacBook-Pro.local>
2025-02-01 23:16:22 +0300

    fix url for going to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const url = "/api/"
+const url = "http://localhost/api/"
 // const url = "http://localhost:4000/"
 
 export const addQuestion = async (question, answers, correctAnswer, description) => {
